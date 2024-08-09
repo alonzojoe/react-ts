6c28f180b539433d65e1aeea3dc3a8e60a48435b
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-08-09 10:03:37 +0800

    feat: Create Components with Type Definition

---
+++ src/components/Todos.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {
+  children?: React.ReactNode;
+  items: string[];
+};
+
+const Todos: React.FC<Props> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
